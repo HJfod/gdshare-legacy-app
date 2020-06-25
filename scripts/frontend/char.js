0b3cd5b0d99f4a8cf1b7ed7e66c2567c168b0fc6
hjfod <dreadrollmusic@gmail.com>
2020-06-25 23:52:08 +0300

    added x svg

--- scripts/frontend/char.js
+++ scripts/frontend/char.js
@@ -1,12 +1,12 @@
-class Checkmark extends HTMLElement {
+class Char extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
         const s = getCSS("--size-svg");
-        this.innerHTML = `<image src='resources/checkmark.svg' width=${s} height=${s} class="svg" />`;
+        this.innerHTML = `<image src='resources/${this.getAttribute("a")}.svg' width=${s} height=${s} class="svg" />`;
     }
 }
 
-customElements.define('c-checkmark', Checkmark);
+customElements.define('c-h', Char);
